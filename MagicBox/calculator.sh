08ce0ec1baea2875c8c92c2dc2a1acf91ec6a228
RabbitTV <154371299+RabbitTV22@users.noreply.github.com>
2025-11-19 09:57:57 -0500

    Change calculator
    Fix power operation handling and improve output messages.

--- MagicBox/calculator.sh
+++ MagicBox/calculator.sh
@@ -8,16 +8,28 @@ if [ $logged_in -eq 1 ]; then
     read -p "Enter the first number: " first
     read -p "Enter the operation (+ - * / ^): " operation
     read -p "Enter the second number: " second
-    if [ $operation == "^" ]; then
-        counter=1
+    if [ "$operation" = "^" ]; then
+        counter=0
         sum=1
-        while [ $counter -lt $second ]; do
+        while [ "$counter" -lt "$second" ]; do
             sum=$(( sum * first ))
             ((counter++))
         done
-        echo $sum
-    elif [ $operation == "+" ] || [ $operation == "-" ] || [ $operation == "*" ] || [ $operation == "/" ]; then
+        echo
+        echo "$first to the power of $second is $sum"
+        echo
+        read -p "Press Enter to return to main menu..."
+        bash $main_menu
+    elif [ "$operation" = "+" ] || [ "$operation" = "-" ] || [ "$operation" = "*" ] || [ "$operation" = "/" ]; then
         sum=$(echo "$first $operation $second" | bc -l)
-        echo $sum
+        echo
+        echo "$first $operation $second is equal to $sum"
+        echo
+        read -p "Press Enter to return to main menu..."
+        bash $main_menu
+    else  
+        echo "You entered an invalid input."
+        read -p "Press Enter to return to main menu..."
+        bash $main_menu
     fi
 fi
